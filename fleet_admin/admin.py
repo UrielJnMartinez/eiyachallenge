eb794e9c0798be722cb6703b59e317d529f47c72
Uriel JnMartinez <urieljnmartinez@gmail.com>
2022-02-19 15:26:52 -0600

    added models into admin site

--- fleet_admin/admin.py
+++ fleet_admin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import City,Vehicle,DistanceBethwenCities
+
+admin.site.register(City)
+admin.site.register(Vehicle)
+admin.site.register(DistanceBethwenCities)
